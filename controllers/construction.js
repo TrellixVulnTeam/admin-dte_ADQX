9e1a85ce61de86034836dafaae284adffe896529
*jgCnKsAjUJ <cm.jung@rsinteractive.co.kr>
2022-08-22 14:34:08 +0900

    editor.datatables ing..

---
+++ controllers/construction.js
@@ -0,0 +1,37 @@
+let db = require("../db");
+let router = require("express").Router();
+let {
+  Editor,
+  Field,
+  Validate,
+  Format,
+  Options,
+  Mjoin,
+} = require("datatables.net-editor-server");
+
+router.all("/api/construction", async function (req, res) {
+  let editor = new Editor(db, "users").fields(
+    new Field("users.name"),
+    new Field("users.signname"),
+    new Field("users.phone"),
+    new Field("users.company_id"),
+    new Field("users.position"),
+    new Field("users.tel"),
+    new Field("users.created_at")
+      .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
+      .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+    new Field("users.updated_at")
+      .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
+      .setFormatter(Format.formatToSqlDate("YYYY-MM-DD"))
+  ).join(
+    new Mjoin("companies").link('users.company_id',"companies.id")
+    .fields(
+      new Field('name')
+    )
+  );
+
+  await editor.process(req.body);
+  res.json(editor.data());
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ let router = require("express").Router();
 
 router.use(require("./staff"));
 router.use(require("./list"));
+router.use(require("./construction"));
 
 module.exports = router;
