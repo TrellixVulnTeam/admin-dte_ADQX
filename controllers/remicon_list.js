5ea30bff1ea209999157bd2b15585a41f1e28c5b
허정준 <jj.heo@rsad.co.kr>
2022-08-23 13:50:46 +0900

    dd

--- controllers/remicon_list.js
+++ controllers/remicon_list.js
@@ -7,6 +7,7 @@ let {
   Format,
   Options,
   Mjoin,
+  SearchPaneOptions,
 } = require("datatables.net-editor-server");
 
 // 레미콘사 get요청
@@ -23,11 +24,12 @@ router.all("/api/list", async function (req, res) {
   console.log(req.query);
   let editor = new Editor(db, "users")
     .fields(
-      new Field("users.signname"),
-      new Field("users.name"),
-      new Field("users.phone"),
-      new Field("users.position"),
-      new Field("users.tel"),
+      new Field("users.id").set(false),
+      new Field("users.signname").searchPaneOptions(new SearchPaneOptions()),
+      new Field("users.name").searchPaneOptions(new SearchPaneOptions()),
+      new Field("users.phone").searchPaneOptions(new SearchPaneOptions()),
+      new Field("users.position").searchPaneOptions(new SearchPaneOptions()),
+      new Field("users.tel").searchPaneOptions(new SearchPaneOptions()),
       new Field("users.created_at")
         // .SetValue(new Date().toISOString())
         .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
@@ -36,17 +38,20 @@ router.all("/api/list", async function (req, res) {
         // .SetValue(new Date().toISOString())
         .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
         .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+      new Field("companies.name"),
       new Field("users.company_id").options(
         new Options().table("companies").value("id").label("name")
       )
+
       // new Field("name")
     )
-    .leftJoin("companies", "users.company_id", "=", "companies.id")
-    .field(
-      new Field("companies.name").options(
-        new Options().table("companies").value("id").label("name")
-      )
-    );
+    .leftJoin("companies", "users.company_id", "=", "companies.id");
+  // .field(
+  //   new Field("companies.name")
+  // .options(
+  //   new Options().table("companies").value("id").label("name")
+  // )
+  // );
   // .join(
   //   new Mjoin("companies")
   //     .link("users.company_id", "companies.id")
@@ -56,55 +61,5 @@ router.all("/api/list", async function (req, res) {
   await editor.process(req.body);
   res.json(editor.data());
 });
-
-// post 요청
-// router.post("/api/list", async function (req, res) {
-//   var body = req.body;
-
-//   // console.log(req.params);
-//   // console.log(req.query.created_at);
-//   // console.log(req.query);
-//   // console.log(req.body.name);
-//   // console.log("body 1", body);
-//   // console.log("body 2", body.data.created_at);
-//   let editor = new Editor(db, "users").fields(
-//     new Field("signname"),
-//     new Field("name"),
-//     new Field("phone"),
-//     new Field("cname"),
-//     new Field("position"),
-//     new Field("tel"),
-//     new Field("created_at")
-//       // .SetValue(new Date().toISOString())
-//       .validator(
-//         Validate.dateFormat(
-//           "YYYY-MM-DD",
-//           null,
-//           new Validate.Options({
-//             message: "Please enter a date in the format yyyy-mm-dd",
-//           })
-//         )
-//       )
-//       .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
-//       .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
-//     new Field("updated_at")
-//       // .set(2)
-//       .setValue(new Date().toISOString())
-//       .validator(
-//         Validate.dateFormat(
-//           "YYYY-MM-DD",
-//           null,
-//           new Validate.Options({
-//             message: "Please enter a date in the format yyyy-mm-dd",
-//           })
-//         )
-//       )
-//       .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
-//       .setFormatter(Format.formatToSqlDate("YYYY-MM-DD"))
-//   );
-
-//   await editor.process(req.body);
-//   res.json(editor.data());
-// });
 
 module.exports = router;
