98296a5983057231eeb7134f672757f023a85d98
*jgCnKsAjUJ <cm.jung@rsinteractive.co.kr>
2022-08-29 18:30:07 +0900

    pull

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,8 @@ router.use(require("./staff"));
 router.use(require("./remicon"));
 router.use(require("./remicon_list"));
 router.use(require("./construction"));
+router.use(require("./construction2"));
 router.use(require("./construction_member"));
-router.use(require("./construction_member2"));
+
 router.use(require("./construction_transaction_ management"));
 module.exports = router;
