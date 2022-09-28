c208e66eaef2e361b823145744475483676c9f9b
*jgCnKsAjUJ <cm.jung@rsinteractive.co.kr>
2022-09-28 14:04:26 +0900

    g

--- controllers/banners.js
+++ controllers/banners.js
@@ -1,6 +1,7 @@
 let db = require("../db");
 let router = require("express").Router();
 let fs = require("fs");
+let moment = require("moment");
 let {
   Editor,
   Field,
@@ -17,7 +18,7 @@ router.all("/api/banners", async function (req, res) {
   var time = new Date();
 
   let editor = new Editor(db, "banners").fields(
-    new Field("id"),
+    new Field("banners.id"),
     new Field("image").setFormatter(Format.ifEmpty(null)).upload(
       new Upload(__dirname + "/../public/uploads/{id}.{extn}")
         .db("banners_files", "id", {
@@ -44,11 +45,69 @@ router.all("/api/banners", async function (req, res) {
     new Field("created_at")
       .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
       .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+    new Field("updated_at")
+      .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
+      .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+
     new Field("banners.active")
   );
   // .where((q) => {
   //   q.where("end_date", ">=", time);
   // });
+  await editor.process(req.body, req.files);
+  res.json(editor.data());
+});
+
+//팝업관리
+router.all("/api/pop_up", async function (req, res) {
+  var time = new Date();
+
+  let editor = new Editor(db, "popup")
+    .fields(
+      new Field("popup.id"),
+      new Field("popup.contents"),
+      new Field("popup.image").setFormatter(Format.ifEmpty(null)).upload(
+        new Upload(__dirname + "/../public/uploads/popup/{id}.{extn}")
+          .db("popup_files", "id", {
+            filename: Upload.Db.FileName,
+            filesize: Upload.Db.FileSize,
+            web_path: "/uploads/popup/{id}.{extn}",
+          })
+          .validator(
+            Validate.fileSize(500000, "Files must be smaller than 500K")
+          )
+          .validator(
+            Validate.fileExtensions(
+              ["png", "jpg", "gif"],
+              "Only image files can be uploaded (png, jpg and gif)"
+            )
+          )
+          .dbClean(async function (data) {
+            for (let i = 0, ien = data.length; i < ien; i++) {
+              await unlink(data[i].system_path);
+            }
+            return true;
+          })
+      ),
+
+      new Field("popup.created_at")
+        .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
+        .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+      new Field("popup.updated_at")
+        .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD"))
+        .setFormatter(Format.formatToSqlDate("YYYY-MM-DD")),
+      new Field("popup.end_date")
+        .getFormatter(Format.sqlDateToFormat("YYYY-MM-DD hh:mm:ss"))
+        .setFormatter(Format.formatToSqlDate("YYYY-MM-DD hh:mm:ss"))
+    )
+    .where((q) => {
+      q.where(
+        "popup.end_date",
+        ">=",
+        moment(time).format("YYYY-MM-DD hh:mm:ss")
+      );
+    });
+
   await editor.process(req.body, req.files);
   res.json(editor.data());
 });
