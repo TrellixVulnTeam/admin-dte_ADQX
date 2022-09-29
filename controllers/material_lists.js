4426f6b89db6992a43ec1065eb1415774b1e0bf7
허정준 <jj.heo@rsad.co.kr>
2022-09-29 17:25:53 +0900

    excel

--- controllers/material_lists.js
+++ controllers/material_lists.js
@@ -1,5 +1,6 @@
 let db = require("../db");
 let router = require("express").Router();
+let fs = require("fs");
 let {
   Editor,
   Field,
@@ -7,6 +8,7 @@ let {
   Format,
   Options,
   Mjoin,
+  Upload,
   SearchPaneOptions,
 } = require("datatables.net-editor-server");
 
@@ -16,13 +18,35 @@ router.all("/api/material_lists", async function (req, res) {
   console.log(req.params.id);
 
   let editor = new Editor(db, "archives").fields(
-    new Field("id"),
-    new Field("type"),
-    new Field("region"),
-    new Field("company_name"),
-    new Field("address"),
-    new Field("tel"),
-    new Field("created_at")
+    new Field("archives.id"),
+    new Field("archives.type"),
+    new Field("archives.region"),
+    new Field("archives.company_name"),
+    new Field("archives.address"),
+    new Field("archives.tel"),
+    new Field("archives.created_at"),
+    new Field("archives.image").setFormatter(Format.ifEmpty(null)).upload(
+      new Upload(__dirname + "/../public/uploads/archives_files/{id}.{extn}")
+        .db("archives_files", "id", {
+          filename: Upload.Db.FileName,
+          filesize: Upload.Db.FileSize,
+          web_path: "/uploads/archives_files/{id}.{extn}",
+          system_path: Upload.Db.SystemPath,
+        })
+        .validator(Validate.fileSize(500000, "Files must be smaller than 500K"))
+        .validator(
+          Validate.fileExtensions(
+            ["xlsx"],
+            "Only image files can be uploaded (xlsx)"
+          )
+        )
+        .dbClean(async function (data) {
+          for (let i = 0, ien = data.length; i < ien; i++) {
+            await unlink(data[i].system_path);
+          }
+          return true;
+        })
+    )
   );
   // .where((q) => {
   //   // q.where("type", "=", req.params.id);
@@ -34,7 +58,7 @@ router.all("/api/material_lists", async function (req, res) {
   //     q.where("type", "=", req.params.id);
   //   }
   // });
-  await editor.process(req.body);
+  await editor.process(req.body, req.files);
   res.json(editor.data());
 });
 
